36c1be1af8fc06b98b00c1fd170d234641a4c4f5
xiao xu, 徐啸 <48782407+xuxiaoooo@users.noreply.github.com>
2024-04-22 15:40:48 +0800

    Add files via upload

--- utils/FusionModel.py
+++ utils/FusionModel.py
@@ -6,22 +6,22 @@ from ImageModel import ImageModel
 from NumModel import NumModel
 
 class FusionModel(nn.Module):
-    def __init__(self, img_model, num_model, lstm_input_size=256, hidden_size=256, num_heads=4, weight_img1=0, weight_img2=0.5, weight_img3=0.5, weight_num=0):
+    def __init__(self, img_model, num_model, lstm_input_size=256, hidden_size=256, num_heads=4, weight_img1=0.1, weight_img2=0.4, weight_img3=0.5, weight_num=0):
         super().__init__()
         self.img_model = img_model
         self.num_model = num_model
 
         self.embedding_adjustment = nn.Linear(128, lstm_input_size)
-        self.lstm = nn.LSTM(input_size=lstm_input_size, hidden_size=hidden_size, num_layers=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size=lstm_input_size, hidden_size=hidden_size, num_layers=3, batch_first=True)
         self.num_features_adjustment = nn.Linear(128, lstm_input_size)
 
         self.my_attention = MyAttention(embed_dim=hidden_size, num_heads=num_heads)
         self.dense_1 = nn.Linear(hidden_size, 128)
         self.batch_normalization_1 = nn.BatchNorm1d(128)
-        self.dropout1 = nn.Dropout(0.5)
+        self.dropout1 = nn.Dropout(0.3)
         self.dense_2 = nn.Linear(128, 64)
         self.batch_normalization_2 = nn.BatchNorm1d(64)
-        self.dropout2 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.3)
         self.dense_3 = nn.Linear(64, 1)
 
         self.weight_img1 = weight_img1
@@ -48,6 +48,103 @@ class FusionModel(nn.Module):
         x = x.transpose(0, 1)
         x = self.my_attention(x)
         x = x.transpose(0, 1)
+        x = x[:, -1, :]
+        x = self.dense_1(x)
+        x = self.batch_normalization_1(x)
+        x = self.dropout1(x)
+        x = self.dense_2(x)
+        x = self.batch_normalization_2(x)
+        x = self.dropout2(x)
+        x = self.dense_3(x)
+        return x
+
+
+class FusionWithoutLSTMModel(nn.Module):
+    def __init__(self, img_model, num_model, lstm_input_size=256, hidden_size=256, num_heads=4, weight_img1=1, weight_img2=1, weight_img3=1, weight_num=1):
+        super().__init__()
+        self.img_model = img_model
+        self.num_model = num_model
+
+        self.embedding_adjustment = nn.Linear(128, lstm_input_size)
+        self.num_features_adjustment = nn.Linear(128, lstm_input_size)
+
+        self.fc1 = nn.Linear(lstm_input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size*2)
+        self.fc3 = nn.Linear(hidden_size*2, hidden_size)
+
+        self.my_attention = MyAttention(embed_dim=hidden_size, num_heads=num_heads)
+        self.dense_1 = nn.Linear(hidden_size, 128)
+        self.batch_normalization_1 = nn.BatchNorm1d(128)
+        self.dropout1 = nn.Dropout(0.3)
+        self.dense_2 = nn.Linear(128, 64)
+        self.batch_normalization_2 = nn.BatchNorm1d(64)
+        self.dropout2 = nn.Dropout(0.3)
+        self.dense_3 = nn.Linear(64, 1)
+
+        self.weight_img1 = weight_img1
+        self.weight_img2 = weight_img2
+        self.weight_img3 = weight_img3
+        self.weight_num = weight_num
+
+    def forward(self, img1, img2, img3, num_features):
+        img1 = self.embedding_adjustment(self.img_model(img1)) * self.weight_img1
+        img2 = self.embedding_adjustment(self.img_model(img2)) * self.weight_img2
+        img3 = self.embedding_adjustment(self.img_model(img3)) * self.weight_img3
+        num_features = self.num_features_adjustment(self.num_model(num_features)) * self.weight_num
+
+        combined = img1 + img2 + img3 + num_features
+        combined = combined / (self.weight_img1 + self.weight_img2 + self.weight_img3 + self.weight_num)
+
+        x = nn.functional.relu(self.fc1(combined))
+        x = nn.functional.relu(self.fc2(x))
+        x = nn.functional.relu(self.fc3(x))
+
+        x = self.my_attention(x.unsqueeze(1))
+        x = x.squeeze(1)
+        x = self.dense_1(x)
+        x = self.batch_normalization_1(x)
+        x = self.dropout1(x)
+        x = self.dense_2(x)
+        x = self.batch_normalization_2(x)
+        x = self.dropout2(x)
+        x = self.dense_3(x)
+        return x
+    
+class FusionWithoutAttentionModel(nn.Module):
+    def __init__(self, img_model, num_model, lstm_input_size=256, hidden_size=256, weight_img1=0, weight_img2=0.5, weight_img3=0.5, weight_num=0):
+        super().__init__()
+        self.img_model = img_model
+        self.num_model = num_model
+
+        self.embedding_adjustment = nn.Linear(128, lstm_input_size)
+        self.lstm = nn.LSTM(input_size=lstm_input_size, hidden_size=hidden_size, num_layers=3, batch_first=True)
+        self.num_features_adjustment = nn.Linear(128, lstm_input_size)
+
+        self.dense_1 = nn.Linear(hidden_size, 128)
+        self.batch_normalization_1 = nn.BatchNorm1d(128)
+        self.dropout1 = nn.Dropout(0.3)
+        self.dense_2 = nn.Linear(128, 64)
+        self.batch_normalization_2 = nn.BatchNorm1d(64)
+        self.dropout2 = nn.Dropout(0.3)
+        self.dense_3 = nn.Linear(64, 1)
+
+        self.weight_img1 = weight_img1
+        self.weight_img2 = weight_img2
+        self.weight_img3 = weight_img3
+        self.weight_num = weight_num
+
+    def forward(self, img1, img2, img3, num_features):
+        img1 = self.embedding_adjustment(self.img_model(img1)) * self.weight_img1
+        img2 = self.embedding_adjustment(self.img_model(img2)) * self.weight_img2
+        img3 = self.embedding_adjustment(self.img_model(img3)) * self.weight_img3
+        num_features = self.num_features_adjustment(self.num_model(num_features)) * self.weight_num
+
+        combined = img1 + img2 + img3 + num_features
+        combined = combined / (self.weight_img1 + self.weight_img2 + self.weight_img3 + self.weight_num)
+
+        batch_size = combined.size(0)
+        combined = combined.view(batch_size, 1, -1)
+        x, _ = self.lstm(combined)
         x = x[:, -1, :]
         x = self.dense_1(x)
         x = self.batch_normalization_1(x)
